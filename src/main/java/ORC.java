15f8f634ad8408fc1eda0002d723d5798dc5d547
rodsouza9 <rodsouza9@gmail.com>
2016-10-14 00:17:44 -0700

    updated junit to 4.12

--- src/main/java/ORC.java
+++ src/main/java/ORC.java
@@ -1,7 +1,19 @@
 /**
  * Created by Rohan D'Souza on 10/13/2016.
  */
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import java.io.*;
+import java.util.Arrays;
+import javax.imageio.*;
 public class ORC {
+    static void noiseReduction(String name) {
+        File image = new File(name);
+
+        int border = 1;
+        for(int i =)
+    }
+
     public static void main(String[] args) {
 
     }
