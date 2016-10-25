2f872444e4e95b400a4ce65ec14bbce548cd4752
Rohan D'Souza <rodsouza9@gmail.com>
2016-10-25 10:42:53 -0700

    thing

--- src/main/java/NoiseReduction.java
+++ src/main/java/NoiseReduction.java
@@ -12,9 +12,15 @@ public class NoiseReduction {
         int width = (border * 2) + 1;
         ArrayList<Color> pixArr = new ArrayList<Color>(width * width);
         BufferedImage img = ImageIO.read(image);
+        int Xwidth, Ywidth;
         for (int i = 0; i < img.getWidth(); i++) {
             for (int j = 0; j < img.getHeight(); j++) {
-
+                Xwidth
+                for (int x = 0; x < width; x++) {
+                    for (int y = 0; y < img.getHeight(); y++) {
+                        width
+                    }
+                }
             }
         }
     }
